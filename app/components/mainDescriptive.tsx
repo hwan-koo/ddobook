0fb8a1b5040fb1e477dff91339b24ded3bf3b346
hwan-koo <korea9kh@naver.com>
2024-03-04 17:51:09 +0900

    0.0.1

--- app/components/mainDescriptive.tsx
+++ app/components/mainDescriptive.tsx
@@ -1,4 +1,4 @@
-export default function (main: string, sub: string) {
+export default function mainDescriptive(main: string, sub: string) {
   return (
     <div className="flex flex-col justify-center items-center">
       <p className="mt-8  font-bold text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl/normal 2xl:text-5xl">
--- app/components/yellowBlueCircle.tsx
+++ app/components/yellowBlueCircle.tsx
@@ -1,4 +1,4 @@
-export default function (number: string) {
+export default function yellowBlueCircle(number: string) {
   return (
     <div className="bg-[#E0FF23] w-[7%] sm:w-[4.2%] aspect-square flex items-center justify-center rounded-full text-[#347CF8] font-bold text-base sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl mt-8 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-32 2xl:mt-40">
       {number}
